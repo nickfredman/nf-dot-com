f94402c5ac9aa17a95df33c307b1826dd2c9df56
Nick Fredman <ntfredman@gmail.com>
2016-02-16 20:45:31 -0800

    finishes template styling

--- my portfolio/assets/js/main.js
+++ my portfolio/assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Hyperspace by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	skel.breakpoints({
@@ -208,3 +202,9 @@
 	});
 
 })(jQuery);
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    setting-name: setting-value
+  });
+});
